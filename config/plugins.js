a38f7eaa4794312a0bf2d042cf980d615e6bac60
Aaron Tribou <aaron.tribou@gmail.com>
2015-07-11 11:43:03 -0500

    Add good and good console for request logging

--- config/plugins.js
+++ config/plugins.js
@@ -1,3 +1,12 @@
 module.exports = [{
   register: require('lout')
+}, {
+  register: require('good'),
+  options: {
+    opsInterval: 5000,
+    reporters: [{
+      reporter: require('good-console'),
+      events: {log: '*', request: '*', response: '*', error: '*'}
+    }]
+  }
 }];
